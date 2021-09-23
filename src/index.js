1d97c3b3d43cfc0008e9d8dfbaba1c960fc5dc11
Javier Jimenez <javierjimenez@Javiers-MacBook-Pro.local>
2021-09-22 21:26:58 -0700

    make it better

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import "./css/App.css";
+import "./css/style.css";
+
+import App from "./App";
+
+// import reportWebVitals from './reportWebVitals';
+
+// button.addEventListener('click', () => {
+//   Twilio.Video.createLocalVideoTrack().then(track => {
+//     container.append(track.attach());
+//     button.remove();
+//   });
+//   button.remove();
+// })
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("body")
 );
 
 // If you want to start measuring performance in your app, pass a function
