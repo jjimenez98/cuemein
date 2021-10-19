bcf3c08d242dadd8dea3615dedf0e5b45a8aef4c
Javier Jimenez <59632440+jjimenez98@users.noreply.github.com>
2021-10-19 16:08:16 -0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  twilio: {
-    accountSid: "AC71013744d21d34272896475247890546",
-    apiKey: "SKc6dbd10e1b9ec0fd5c3606461c6ceff3",
-    apiSecret: "Xvv9pop4eMToKUCFwBPYWYTtw7QWz3iR",
-  },
-};
